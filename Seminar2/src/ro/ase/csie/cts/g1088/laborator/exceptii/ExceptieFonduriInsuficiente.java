6fbeac387509a80b0713bde5970c63e45024a1e4
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-03-04 12:22:49 +0200

    partea 1

---
+++ Seminar2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception{
+
+    public ExceptieFonduriInsuficiente(String mesaj){
+        super(mesaj);
+    }
+
+    public ExceptieFonduriInsuficiente(){
+
+    }
+
+}
---
+++ Seminar2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieTransferIlegal.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1088.laborator.exceptii;
+
+public class ExceptieTransferIlegal extends Exception{
+
+    public ExceptieTransferIlegal(String mesaj){
+        super(mesaj);
+    }
+
+    public ExceptieTransferIlegal(){
+
+    }
+
+}
