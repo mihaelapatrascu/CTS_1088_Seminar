4b18d463108eec180fa0e78cc7df69fc55b2416c
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-03-11 13:30:48 +0200

    commit final

--- Seminar3/src/ro/ase/csie/cts/g1088/laborator3/faza2/Produs.java
+++ Seminar3/src/ro/ase/csie/cts/g1088/laborator3/faza2/Produs.java
@@ -4,51 +4,63 @@ import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
 import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
 
 public class Produs {
+
     public static final int VECHIME_CLIENT_MAXIMA = 10;
-    public static final float DISCOUNT_CLIENT_MAXIM = (float) 0.15;
+    public static final float DISCOUNT_CLIENT_MAXIM = 0.15f;
 
     public static void validarePret(float pretInitial) throws ExceptiePretInvalid {
-        if(pretInitial <= 0){
+        if(pretInitial <= 0) {
             throw new ExceptiePretInvalid();
         }
     }
 
-    public static void validareVechime(int vechimeClientInAni) throws ExceptieVechimeClient {
-        if(vechimeClientInAni < 0){
+    public static void validareVechimeClient(int vechimeClientInAni) throws ExceptieVechimeClient {
+        if(vechimeClientInAni < 0) {
             throw new ExceptieVechimeClient();
         }
     }
 
-    public static float getDiscountFidelitate(int vechimeClientInAni){
+    public static float getDiscountFidelitate(int vechimeClientInAni) {
         return (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM : (float)vechimeClientInAni/100;
     }
 
-    public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni) throws ExceptiePretInvalid, ExceptieVechimeClient {
-        validarePret(pretInitial);
-        validareVechime(vechimeClientInAni);
+    public static float getPretCuDiscount(float pretInitial, float discount) {
+        return pretInitial - (discount * pretInitial);
+    }
 
-        float discount = TipProdus.NOU.getDiscount();
+    public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni)
+            throws ExceptiePretInvalid, ExceptieVechimeClient
+    {
+
+        validarePret(pretInitial);
+        validareVechimeClient(vechimeClientInAni);
+
         float pretFinal = 0;
         float discountFidelitate = getDiscountFidelitate(vechimeClientInAni);
+        float discount = TipProdus.NOU.getDiscount();
+        float valoareDiscountTipProdus = 0;
 
-        switch (tipProdus){
+        switch(tipProdus) {
             case NOU:
                 pretFinal = pretInitial;
                 break;
             case DISCOUNT:
                 discount = TipProdus.DISCOUNT.getDiscount();
-                pretFinal = (pretInitial - (discount * pretInitial)) - discountFidelitate * (pretInitial - (discount* pretInitial));
+                valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
+                pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
                 break;
             case STOC_LIMITAT:
                 discount = TipProdus.STOC_LIMITAT.getDiscount();
-                pretFinal = (pretInitial - (discount * pretInitial)) - discountFidelitate * (pretInitial - (discount * pretInitial));
+                valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
+                pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
                 break;
             case VECHI:
                 discount = TipProdus.VECHI.getDiscount();
-                pretFinal = (pretInitial - (discount * pretInitial)) - discountFidelitate * (pretInitial - (discount * pretInitial));
+                valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
+                pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
                 break;
             default:
-                throw new UnsupportedOperationException("Un simbol din enumerare nu este procesat!");
+                throw new UnsupportedOperationException("Un simbol din enumerare nu este procesat");
         }
 
         return pretFinal;
