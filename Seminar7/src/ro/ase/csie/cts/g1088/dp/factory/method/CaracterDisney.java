b3d93cb1a860fb97090da5830cf46413fbb01684
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-04-08 12:35:30 +0300

    commit pauza seminar 7

---
+++ Seminar7/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterDisney.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public class CaracterDisney extends CaracterJoc{
+
+    public CaracterDisney(String nume) {
+        this.nume = nume;
+        this.putere = 200;
+    }
+
+    @Override
+    public void seDeplaseaza() {
+
+    }
+}
