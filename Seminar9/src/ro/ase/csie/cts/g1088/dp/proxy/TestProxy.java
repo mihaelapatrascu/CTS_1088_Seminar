7a317b6ac1357d806441862a9e91f9c8fc2c6a30
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-04-22 13:22:38 +0300

    commit proxy seminar 9

---
+++ Seminar9/src/ro/ase/csie/cts/g1088/dp/proxy/TestProxy.java
@@ -0,0 +1,26 @@
+package ro.ase.csie.cts.g1088.dp.proxy;
+
+import java.util.SortedMap;
+
+public class TestProxy {
+
+    public static void main(String[] args) {
+
+        InterfataLogin login = new ModulLogin("10.10.0.1");
+        if(login.login("admin", "root1234"))
+            System.out.println("Hello admin!");
+
+        login = new ProxyLogin(login);
+
+        String[] parole =
+                new String[] {"1234", "admin", "root", "root12", "root1234"};
+
+        for(String parola : parole) {
+            if(login.login("admin", parola))
+                System.out.println("Am gasit parola: " + parola);
+        }
+
+        System.out.println("Game over!");
+
+    }
+}
