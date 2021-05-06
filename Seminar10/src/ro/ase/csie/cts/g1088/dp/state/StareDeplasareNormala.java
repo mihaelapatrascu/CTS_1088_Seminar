88373d09522346bcb8db73b8622d9122457b8036
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-05-06 13:31:11 +0300

    commit final seminar 10

---
+++ Seminar10/src/ro/ase/csie/cts/g1088/dp/state/StareDeplasareNormala.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1088.dp.state;
+
+public class StareDeplasareNormala implements InterfataModDeplasare{
+
+    @Override
+    public void seMisca(SuperErou erou) {
+        System.out.println(erou.nume + " alearga");
+    }
+}
