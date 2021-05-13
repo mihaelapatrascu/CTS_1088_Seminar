421ac1b949caacea1e193440d741dc828b492310
Mihaela Patrascu <patrascumihaela379@yahoo.com>
2021-05-13 12:24:46 +0300

    commit chain seminar 11

---
+++ Seminar11/src/ro/ase/csie/cts/g1088/dp/chain/ModulMesajePrivate.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.cts.g1088.dp.chain;
+
+import java.util.Locale;
+
+public class ModulMesajePrivate extends ModulProcesareMesaj {
+
+    @Override
+    public void procesareMesaj(MesajChat mesaj) {
+
+        if (!mesaj.destinatie.toLowerCase().equals("@everyone")) {
+            System.out.println("Mesaj privat pentru: " + mesaj.destinatie + " : " + mesaj.text);
+        }
+        else {
+            if (this.next != null) {
+                this.next.procesareMesaj(mesaj);
+            }
+        }
+    }
+}
